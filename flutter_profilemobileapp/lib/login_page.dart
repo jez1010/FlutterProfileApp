08f497480e5af78295090796bf7bba6ba7cd7b66
jez1010 <grsncrdn88@gmail.com>
2026-01-27 09:51:12 +0800

    Added a GIF background. To be modifed.

--- flutter_profilemobileapp/lib/login_page.dart
+++ flutter_profilemobileapp/lib/login_page.dart
@@ -42,174 +42,211 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Color(0xFF232738),
 
       body: Container(
-        height: double.infinity,
-
-        margin: EdgeInsets.all(25),
-
-        clipBehavior: Clip.antiAlias,
-
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.centerRight,
-
-            colors: [Color(0xFF2D27D7), Color(0xFF191755)],
+          image: DecorationImage(
+            image: NetworkImage('https://media1.tenor.com/m/Al8FHubXhZsAAAAd/max-verstappen-donuts.gif'),
+            fit: BoxFit.cover,
           ),
-
-          boxShadow: [
-            BoxShadow(
-              color: Color(0xBF000000),
-              blurRadius: 7,
-            )
-          ]
         ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 450,
+                width: 350,
 
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            double containerHeight = constraints.maxHeight * 0.75;
-            double iconPosition = constraints.maxHeight * 0.12;
+                clipBehavior: Clip.antiAlias,
 
-            return Stack(
-              children: [
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: containerHeight,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
 
-                    padding: EdgeInsets.all(20),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.centerRight,
 
-                    alignment: Alignment.bottomCenter,
+                    colors: [Color(0xFF2D27D7), Color(0xFF191755)],
+                  ),
 
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Color(0xFFFFFFFF),
-                    ),
+                  boxShadow: [
+                    BoxShadow(color: Color(0xBF000000), blurRadius: 7),
+                  ],
+                ),
 
-                    child: Container(
-                      padding: EdgeInsets.only(top: 75),
-                      child: Column(
-                        children: [
-                          Text(
-                            "LOGIN",
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              fontFamily: 'IBMPlexSans',
-                              fontWeight: FontWeight.w600,
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    double containerHeight = constraints.maxHeight * 0.75;
+                    double iconPosition = constraints.maxHeight * 0.12;
+
+                    return Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            height: containerHeight,
+
+                            padding: EdgeInsets.all(20),
+
+                            alignment: Alignment.bottomCenter,
+
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color(0xFFFFFFFF),
                             ),
-                          ),
 
-                          //The Actual Login Form
-                          Container(
-                            child: Form(
-                              key: _loginForm,
+                            child: Container(
+                              padding: EdgeInsets.only(top: 75),
                               child: Column(
                                 children: [
-                                  TextFormField(
-                                    decoration: InputDecoration(
-                                      labelText: 'Email',
-                                      hintText: 'Enter email here.',
+                                  Text(
+                                    "LOGIN",
+                                    style: TextStyle(
+                                      fontSize: 30.0,
+                                      fontFamily: 'IBMPlexSans',
+                                      fontWeight: FontWeight.w600,
                                     ),
-
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Input required.';
-                                      }
-                                      return null;
-                                    },
                                   ),
 
-                                  TextFormField(
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                      labelText: 'Password',
-                                      hintText: 'Enter your password here.',
-                                    ),
+                                  //The Actual Login Form
+                                  Container(
+                                    child: Form(
+                                      key: _loginForm,
+                                      child: Column(
+                                        children: [
+                                          TextFormField(
+                                            decoration: InputDecoration(
+                                              labelText: 'Email',
+                                              hintText: 'Enter email here.',
+                                            ),
 
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Input required.';
-                                      }
-                                      return null;
-                                    },
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return 'Input required.';
+                                              }
+                                              return null;
+                                            },
+                                          ),
+
+                                          TextFormField(
+                                            obscureText: true,
+                                            decoration: InputDecoration(
+                                              labelText: 'Password',
+                                              hintText:
+                                                  'Enter your password here.',
+                                            ),
+
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return 'Input required.';
+                                              }
+                                              return null;
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+
+                                  Container(
+                                    margin: EdgeInsets.all(20),
+
+                                    alignment: Alignment.center,
+
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        backgroundColor: Color(0xFF0F0A8F),
+                                        disabledForegroundColor: Color(
+                                          0xFF030231,
+                                        ),
+
+                                        foregroundColor: Color(0xFFFFFFFF),
+
+                                        textStyle: TextStyle(
+                                          fontSize: 15.0,
+                                          fontFamily: 'IBMPlexSans',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            10,
+                                          ),
+                                        ),
+                                      ),
+
+                                      child: Text('Submit'),
+
+                                      onPressed: () {
+                                        if (_loginForm.currentState!
+                                            .validate()) {
+                                          showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) =>
+                                                AlertDialog(
+                                                  title: const Text(
+                                                    'Form Completed',
+                                                  ),
+                                                  content: Text(
+                                                    'All fields completed. Confirm?',
+                                                  ),
+                                                  actions: [
+                                                    TextButton(
+                                                      onPressed: () =>
+                                                          Navigator.pop(
+                                                            context,
+                                                            'Cancel',
+                                                          ),
+                                                      child: Text('Cancel'),
+                                                    ),
+                                                    TextButton(
+                                                      onPressed: () =>
+                                                          Navigator.pop(
+                                                            context,
+                                                            'OK',
+                                                          ),
+                                                      child: Text('OK'),
+                                                    ),
+                                                  ],
+                                                ),
+                                          );
+                                        }
+                                      },
+                                    ),
                                   ),
                                 ],
                               ),
                             ),
                           ),
-                          
-                          Container( 
-                            margin: EdgeInsets.all(20),
+                        ),
 
-                            alignment: Alignment.center, 
+                        Positioned(
+                          top: iconPosition,
+                          left: 0,
+                          right: 0,
 
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xFF0F0A8F),
-                              ),
-
-                              child: Text(
-                                'Submit',
-                                style: TextStyle(
-                                  fontSize: 15.0,
-                                  fontFamily: 'IBMPlexSans',
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ), 
-
-                              onPressed: () { 
-                                if (_loginForm.currentState!.validate()) { 
-                                  showDialog( 
-                                    context: context, 
-                                    builder: (BuildContext context) => AlertDialog( 
-                                      title: const Text('Form Completed'), 
-                                      content: Text('All fields completed. Confirm?'), 
-                                      actions: [ 
-                                        TextButton( 
-                                          onPressed: () => Navigator.pop(context, 'Cancel'), 
-                                          child: Text('Cancel'), 
-                                        ), 
-                                        TextButton( 
-                                          onPressed: () => Navigator.pop(context, 'OK'), 
-                                          child: Text('OK'), 
-                                        ), 
-                                      ] 
-                                    ) 
-                                  ); 
-                                } 
-                              } 
-                            ), 
-                          ) 
-
-                        ],
-                      ),
-                    ),
-                  ),
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFF232738),
+                            ),
+                            child: Icon(
+                              Icons.account_circle_sharp,
+                              size: 150,
+                              color: Color(0xFFFFFFFF),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 ),
-
-                Positioned(
-                  top: iconPosition,
-                  left: 0,
-                  right: 0,
-
-                  child: Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFF232738),
-                    ),
-                    child: Icon(
-                      Icons.account_circle_sharp,
-                      size: 150,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          },
+              ),
+            ],
+          ),
         ),
       ),
     );
