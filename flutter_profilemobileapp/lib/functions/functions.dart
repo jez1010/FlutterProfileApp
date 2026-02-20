e7313f32c11ccc22c9c097e56af0038ccc576a41
jez1010 <grsncrdn88@gmail.com>
2026-02-20 10:27:36 +0800

    Added another SafeArea. Increased padding for profile_page to grab things.

--- flutter_profilemobileapp/lib/functions/functions.dart
+++ flutter_profilemobileapp/lib/functions/functions.dart
@@ -7,7 +7,7 @@ import '../main.dart';
 import '../screens/login_page.dart';
 
 //grab current user
-String getUser(){
+String getUser() {
   final user = supabase.auth.currentUser;
   if (user != null) {
     final userId = user.id;
@@ -22,7 +22,7 @@ class RegisteringNewUsers {
     String email,
     String password,
     String username, //varchar
-    String firstName,//varchar
+    String firstName, //varchar
     String lastName, //varchar
     String profilePicture,
   ) async {
@@ -35,7 +35,7 @@ class RegisteringNewUsers {
           'first_name': firstName,
           'last_name': lastName,
           'photo_link': profilePicture,
-        }
+        },
       );
 
       return null;
@@ -43,7 +43,7 @@ class RegisteringNewUsers {
       return e.code ?? e.message;
     } catch (e) {
       print("$e");
-      return(e.toString());
+      return (e.toString());
     }
   }
 }
@@ -56,23 +56,30 @@ class ProfileRepository {
     return _cachedProfile;
   }
 
-  Future<List<dynamic>> getProfileDetails({String? personId}) async{
-
+  Future<List<dynamic>> getProfileDetails({String? personId}) async {
     final userId = supabase.auth.currentUser?.id;
 
     final String? targetId = personId ?? userId;
 
     if (targetId == null) return [];
-    if (personId == null && _cachedProfile != null && targetId == userId) return _cachedProfile!;
+    if (personId == null && _cachedProfile != null && targetId == userId)
+      return _cachedProfile!;
 
     final response = await supabase
-      .from('PROFILE')
-      .select()
-      .eq('user_id', targetId)
-      .maybeSingle();
+        .from('PROFILE')
+        .select()
+        .eq('user_id', targetId)
+        .maybeSingle();
 
     if (response == null) {
-      return [userId, "Default Username", "Default Name", "Default Social Media", "Default Description", "Default Photo URL"];
+      return [
+        userId,
+        "Default Username",
+        "Default Name",
+        "Default Social Media",
+        "Default Description",
+        "Default Photo URL",
+      ];
     }
 
     String name1 = response['first_name'] ?? "";
@@ -83,7 +90,7 @@ class ProfileRepository {
     } else {
       names = name1.isNotEmpty ? "$name1|" : "|$name2";
     }
-    
+
     final List<dynamic> details = [
       targetId,
       response['username'],
@@ -107,7 +114,7 @@ class ProfileRepository {
   List<String> parseTags(String tagString) {
     List<String> tags = tagString.split(',');
 
-    for (int i = 0; i <tags.length; i++) {
+    for (int i = 0; i < tags.length; i++) {
       tags[i] = tags[i].trim();
     }
 
@@ -115,18 +122,22 @@ class ProfileRepository {
   }
 
   Map<String, dynamic> parseSchool(Map<String, dynamic> decodedJson) {
-    if(decodedJson.containsKey('education') && decodedJson['education'] is Map) {
+    if (decodedJson.containsKey('education') &&
+        decodedJson['education'] is Map) {
       return decodedJson['education'] as Map<String, dynamic>;
     }
     return {};
   }
 
-  Future<void> pushToDatabase(List<dynamic> inputs, BuildContext context) async {
-    // 0 = username, 
-    //1 = first name, 
-    //2 = last name, 
-    //3 = tags, 
-    //4 = desc, 
+  Future<void> pushToDatabase(
+    List<dynamic> inputs,
+    BuildContext context,
+  ) async {
+    // 0 = username,
+    //1 = first name,
+    //2 = last name,
+    //3 = tags,
+    //4 = desc,
     //5 = contacts
     //6 = pfp
 
@@ -144,19 +155,16 @@ class ProfileRepository {
     };
 
     try {
-      await supabase
-        .from('PROFILE')
-        .update(newData)
-        .eq('user_id', userId);
+      await supabase.from('PROFILE').update(newData).eq('user_id', userId);
 
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Profile updated successfully."))
+        const SnackBar(content: Text("Profile updated successfully.")),
       );
     } catch (e) {
       print(e);
       if (context.mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Error saving data. Try again later."))
+          const SnackBar(content: Text("Error saving data. Try again later.")),
         );
       }
     }
@@ -164,59 +172,58 @@ class ProfileRepository {
     clearCache();
     getProfileDetails();
   }
-
 }
 
 Widget standardDrawer(BuildContext context) {
   return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: [
-        ListTile(
-          leading: Icon(Icons.person),
-          title: Text("My Profile"),
-          onTap: () async {
-            Navigator.pop(context);
+    child: SafeArea(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("My Profile"),
+            onTap: () async {
+              Navigator.pop(context);
 
-            if(context.mounted){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfileScreen())
-              );
-            }
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.search),
-          title: Text("Search"),
-          onTap: () async {
-            Navigator.pop(context);
+              if (context.mounted) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfileScreen()),
+                );
+              }
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.search),
+            title: Text("Search"),
+            onTap: () async {
+              Navigator.pop(context);
 
-            if(context.mounted){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SearchScreen())
-              );
-            }
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.settings),
-          title: const Text("About"),
-          onTap: () {
-            Navigator.pop(context);
+              if (context.mounted) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchScreen()),
+                );
+              }
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("About"),
+            onTap: () {
+              Navigator.pop(context);
 
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  // This removes the default rounded corners
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero,
-                  ),
-                  title: const Text("About This App"),
-                  content: (
-                    Column(
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    // This removes the default rounded corners
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                    title: const Text("About This App"),
+                    content: (Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -225,60 +232,63 @@ Widget standardDrawer(BuildContext context) {
                         SizedBox(height: 5),
                         Text(
                           "Changelogs (1.0.1):",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                          )
+                          style: TextStyle(fontWeight: FontWeight.w500),
                         ),
-                        Text("> Fixed user registration.")
+                        Text("> Fixed user registration."),
                       ],
-                    )
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("CLOSE"),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.logout),
-          title: Text("Logout"),
-          onTap: () async {
-            ProfileRepository.clearCache();
-
-            await supabase.auth.signOut();
-
-            Navigator.pop(context);
-
-            if (context.mounted) {
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginScreen()),
-                (route) => false,
+                    )),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text("CLOSE"),
+                      ),
+                    ],
+                  );
+                },
               );
-            }
-          },
-        ),
-      ],
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () async {
+              ProfileRepository.clearCache();
+
+              await supabase.auth.signOut();
+
+              Navigator.pop(context);
+
+              if (context.mounted) {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  (route) => false,
+                );
+              }
+            },
+          ),
+        ],
+      ),
     ),
   );
 }
 
-Future<Map<String, List<dynamic>>> searchProfiles({String? user_id, String? term}) async {
+Future<Map<String, List<dynamic>>> searchProfiles({
+  String? user_id,
+  String? term,
+}) async {
   try {
     final List<dynamic> response = await supabase.rpc(
       'searchprofiles',
-      params: {'p_user_id': user_id, 'p_search_term': term}
+      params: {'p_user_id': user_id, 'p_search_term': term},
     );
 
     final List<List<dynamic>> structuredResults = response.map((profile) {
       String name1 = profile['first_name'] ?? "";
       String name2 = profile['last_name'] ?? "";
-      String names = (name1.isNotEmpty || name2.isNotEmpty) ? "$name1|$name2" : "$name1|$name2";
+      String names = (name1.isNotEmpty || name2.isNotEmpty)
+          ? "$name1|$name2"
+          : "$name1|$name2";
 
       return [
         profile['user_id'],
@@ -293,9 +303,7 @@ Future<Map<String, List<dynamic>>> searchProfiles({String? user_id, String? term
     }).toList();
 
     // 3. Wrap the List of Lists in a Map
-    return {
-      'profiles': structuredResults,
-    };
+    return {'profiles': structuredResults};
   } catch (e) {
     print("Search error: $e");
     return {'profiles': []};
